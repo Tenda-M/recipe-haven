91ed70b4d36ce6fee38ad6444a7b78f0d3e379ac
Tatenda Mudehwe <tmudehwe@hotmail.com>
2024-09-07 19:02:11 +0000

    Update settings for deployment readiness

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7pwjk10o#2xzfz0+34fe56d1t32(97!f1m%8eli1di9rwl)l-='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["8000-tendam-recipehaven-jewf8wckm85.ws.codeinstitute-ide.net"]
+ALLOWED_HOSTS = ["8000-tendam-recipehaven-jewf8wckm85.ws.codeinstitute-ide.net",".herokuapp.com"]
 
 
 # Application definition
